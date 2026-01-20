a8daf7de70557e07cada0bfc4cba511a0f4e24c4
jacobo.s.amador@gmail.com <jacobo.s.amador@gmail.com>
2026-01-20 15:28:36 -0500

    unstable/gesture_control_implementation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,13 +7,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <QueryClientProvider client={queryClient}>
-        <RootNavigator />
-      </QueryClientProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <QueryClientProvider client={queryClient}>
+          <RootNavigator />
+        </QueryClientProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
